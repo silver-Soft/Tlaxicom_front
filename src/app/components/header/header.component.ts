8e3832bc254ca99d21ede739a48f28e20494f69a
SilverLóz <slpezromano.sl@gmail.com>
2025-05-14 15:04:52 -0600

    Initial commit
    Nuevo proyecto Tlaxicom en angular

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatIconModule, RouterModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
